8271a3c43538792242cd49c5990a59ba5a3002e3
Carl Christensson <carl.e.christensson@gmail.com>
2020-04-06 15:00:59 +0200

    Version two

--- src/main/java/se/lexicon/carl/App.java
+++ src/main/java/se/lexicon/carl/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hallo Wurld! from Carl.");
+
+        int inputSeconds = 86399;
+        int excess = inputSeconds % 3600;
+        int hours = (inputSeconds - excess)/3600;
+        int minutes = excess % 60;
+        int seconds = minutes % 60;
+
+        System.out.println(hours + "." + minutes + ":" + seconds);
     }
 }
